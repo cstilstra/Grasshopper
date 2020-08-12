826d6e732e7b5f42a50485680f610d74d3d4218b
c.s.tilstra <cstilstra@gmail.com>
2020-08-11 21:20:03 -0700

    add adder.

---
+++ Grasshopper/ClientApp/src/components/Adder.js
@@ -0,0 +1,33 @@
+﻿import React, { Component } from 'react';
+
+export class Adder extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            firstNumber: 0,
+            secondNumber: 0
+        }
+
+        this.firstNumberChanged = this.firstNumberChanged.bind(this)
+        this.secondNumberChanged = this.secondNumberChanged.bind(this)
+    }
+
+    firstNumberChanged(event) {
+        this.setState({ firstNumber: event.target.value })
+    }
+
+    secondNumberChanged(event) {
+        this.setState({ secondNumber: event.target.value })
+    }
+
+    render(){
+        return (
+            <div>
+                <input type="number" value={this.state.firstNumber} onChange={this.firstNumberChanged}/>
+                <input type="number" value={this.state.secondNumber} onChange={this.secondNumberChanged}/>
+                <h4>{this.state.firstNumber + this.state.secondNumber}</h4>
+            </div>
+        );
+    }
+}
