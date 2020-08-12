8448711d0937856e20103c208ab1d77b986bdb6b
c.s.tilstra <cstilstra@gmail.com>
2020-08-11 20:44:10 -0700

    remove old project, create new react project.

--- Grasshopper/Program.cs
+++ Grasshopper/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace GrasshopperAPI
+namespace Grasshopper
 {
     public class Program
     {
--- GrasshopperAPI/Models/Grasshopper.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GrasshopperAPI.Models
-{
-    public class Grasshopper
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
